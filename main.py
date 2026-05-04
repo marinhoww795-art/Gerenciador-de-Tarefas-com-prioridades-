237cb86760c57e6391ea1953f1910bb9cb893182
Gabriel Marinho do Nascimento <marinhoww795@gmail.com>
2026-05-04 12:12:40 -0300

    Mudança

---
+++ main.py
@@ -0,0 +1,13 @@
+pendentes = []
+while True:
+    print("--- Gerenciador de Tarefas ---")
+    print("""(1) Usuario\n(2) Tarefas\n\n""")
+    escolha = int(input("\nDigite a opção: "))
+    
+
+
+
+
+
+
+
